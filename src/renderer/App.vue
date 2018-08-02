e0c192de64d894d0458738780c58868bb2b96078
王士江 <wangshijiang@icourt.cc>
2018-08-02 20:37:23 +0800

    添加聊天记录查看的基本样式

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -12,4 +12,25 @@
 
 <style>
   /* CSS */
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    color: #4a4a4a;
+    -webkit-font-smoothing: antialiased;
+    overflow: hidden;
+  }
+  body, button, input, pre, textarea {
+    font-family: PingFang SC,Hiragino Sans GB,Microsoft Yahei,sans-serif;
+  }
+  .ichat, .vd-approval-detail, body, html {
+    height: 100%;
+  }
+  li, ul {
+    list-style: none;
+  }
 </style>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import axios from 'axios';
+import Element from 'element-ui/lib';
+import 'element-ui/lib/theme-chalk/index.css';
 
 import App from './App';
 import router from './router';
 import store from './store';
 
+Vue.use(Element);
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
