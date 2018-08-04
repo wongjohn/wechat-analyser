81eaff2cfd2f1a89d28a8b3b525d9bbf650edbd4
王士江 <wangshijiang@icourt.cc>
2018-08-04 18:48:10 +0800

    添加菜单

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron' // eslint-disable-line
+import { app, BrowserWindow, Menu } from 'electron' // eslint-disable-line
 const path = require('path');
 /**
  * Set `__static` path to static files in production
@@ -13,7 +13,77 @@ const winURL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:9080'
   : `file://${__dirname}/index.html`;
 
+// 全局右键
+require('electron-context-menu')({
+  showInspectElement: false,
+  labels: {
+    copy: '复制',
+    paste: '粘贴',
+    copyLink: '复制链接地址',
+    cut: '剪切',
+    save: '保存图片',
+  },
+});
+
+function setMenu() {
+  // 设置菜单
+  const template = [
+    {
+      label: 'AlphaTeam',
+      submenu: [
+        {
+          label: '退出',
+          accelerator: 'Command+Q',
+          click() {
+            app.quit();
+          },
+        },
+      ],
+    },
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: '关闭窗口',
+          accelerator: 'CmdOrCtrl+W',
+          click() {
+            const focusedWindow = BrowserWindow.getFocusedWindow();
+            focusedWindow.close();
+          },
+        },
+      ],
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: '撤销', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+        { label: '回退', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: '剪切', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+        { label: '复制', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+        { label: '粘贴', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        { label: '全选', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
+      ],
+    },
+    {
+      label: '开发者',
+      submenu: [
+        {
+          label: '打开控制台',
+          click() {
+            mainWindow.webContents.openDevTools({
+              mode: 'detach',
+            });
+          },
+        },
+      ],
+    },
+  ];
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
+
 function createWindow() {
+  setMenu();
   /**
    * Initial window options
    */
@@ -29,10 +99,20 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadURL(winURL);
+  mainWindow.loadURL(winURL, { httpReferrer: '' });
 
   mainWindow.on('closed', () => {
     mainWindow = null;
+  });
+
+  const filter = {
+    urls: [],
+  };
+
+  mainWindow.webContents.session.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
+    const { requestHeaders } = details;
+    Object.assign(requestHeaders, { Referer: '' });
+    callback({ cancel: false, requestHeaders });
   });
 }
 
