0ea4030144f6ecd71a63ef8bcc7d3eb431a41bc7
王士江 <wangshijiang@icourt.cc>
2018-08-16 19:20:44 +0800

    模块添加"计算器"，文档生成完毕、打开文档所在的文件夹

--- src/renderer/service/bug-service.js
+++ src/renderer/service/bug-service.js
@@ -2,6 +2,7 @@ import { Message } from 'element-ui/lib';
 const Excel = require('exceljs');
 const path = require('path');
 const os = require('os');
+const exec = require('child_process').exec;
 const readFilePath = path.resolve(__dirname, 'alpha__bug_template.xlsx');
 
 let bugs = [];
@@ -11,7 +12,7 @@ const MODULES = (['wordalpha', 'note', '安装指引', '登录', '账号', 'Chro
   '律助', '文档', 'alphabox', '客户', '行业雷达', '利冲', '合同库', '项目模板', '任务模板',
   '合同模板', '文书模板', '权限', '部门', '安卓', 'iOS', '享聊', '通知', 'km', '客户端',
   '成就分析', '财务', '报表', '审批', '职级', '帮助中心', '设置', 'chrome插件', '系统',
-  '橙信', '培训', '课程', '其他'].map(module => ({ label: module, value: module })));
+  '橙信', '培训', '课程', '计算器', '其他'].map(module => ({ label: module, value: module })));
 
 const TYPES = [
   { label: '缺陷', value: '缺陷' },
@@ -31,10 +32,12 @@ function exportBugs() {
         bugs.forEach((bug) => {
           worksheet.addRow([bug.module, bug.title, bug.type, bug.source, bug.detail]);
         });
-        workbook.xlsx.writeFile(path.resolve(os.homedir(), `Downloads/Alpha-Bugs-${(new Date()).getTime()}.xlsx`));
+        const genDir = path.resolve(os.homedir(), `Downloads/Alpha-Bugs-${(new Date()).getTime()}.xlsx`);
+        workbook.xlsx.writeFile(genDir);
         bugs = [];
         Message.success('Bug文档生成完毕');
         resolve(bugs);
+        exec('open .', { cwd: path.dirname(genDir) });
       }, (error) => {
         reject(error);
       });
